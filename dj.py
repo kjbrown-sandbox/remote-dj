3110e3f270e4b96418bdba4d063bfe10cd22fb70
Kimberly Brown <kjbrown.home@gmail.com>
2023-10-16 11:31:52 -0600

    Now plays sound when certain keys are pressed

--- dj.py
+++ dj.py
@@ -1,9 +1,13 @@
 from pynput import keyboard
+from playsound import playsound
+
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        # print('alphanumeric key {0} pressed'.format(
+        #     key.char))
+        if key == keyboard.KeyCode.from_char('a'):
+            playsound('sounds/Wilhelm Scream Remastered.wav')
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
